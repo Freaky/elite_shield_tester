0ec1e56cdf6da8516320718e553c8e504c236dea
Thomas Hurst <tom@hur.st>
2019-11-07 19:06:55 +0000

    Add FDL and Cutter shield configs
    Will generalise this later using Coriolis data

--- www/index.php
+++ www/index.php
@@ -4,6 +4,12 @@ define('TESTER_PATH', "/home/freaky/elite_shield_tester/");
 define('LOCK_PATH', '/tmp/shield_tester.lock');
 define('OWN_URL', '/shieldtester/');
 
+$Ships = array(
+  'Anaconda' => null,
+  'Fer-De-Lance' => 'data/ShieldGeneratorVariants_FDL.csv',
+  'Imperial Cutter' => 'data/ShieldGeneratorVariants_Cutter.csv',
+);
+
 function try_lock($kind = "") {
   $fp = @fopen(LOCK_PATH . $kind, "w");
   if ($fp && @flock($fp, LOCK_EX)) {
@@ -17,6 +23,8 @@ function release_lock($fp) {
 }
 
 function try_calculate() {
+  global $Ships;
+
   $path = TESTER_PATH;
   $flags = array(
     'dps_thermal' => '-t',
@@ -55,6 +63,10 @@ function try_calculate() {
   $trim = 0;
   $kind = ".fast";
 
+  if (isset($_GET['ship']) && $csv = $Ships[$_GET['ship']]) {
+    $args[] = "--shield-csv " . TESTER_PATH . $csv;
+  }
+
   if (!empty($args) && !isset($_GET['prismatics'])) {
     $args[] = "--disable-prismatic";
   }
@@ -66,7 +78,8 @@ function try_calculate() {
 
   if ($boosters > 6) {
     // These never seem to help
-    $args[] = "--force-experimental";
+    // (Yes they do, on fast regen builds)
+    // $args[] = "--force-experimental";
     $kind = ".$boosters";
   }
 
@@ -77,6 +90,7 @@ function try_calculate() {
       $args = implode(" ", $args);
       @exec("$path/elite_shield_tester $args", $result);
       $result = implode("\n", array_slice($result, $trim));
+      $result = str_replace(TESTER_PATH, "", $result);
       release_lock($l);
     } else {
       $result = "Couldn't get a lock. Try later :(";
@@ -106,6 +120,23 @@ function num_input($name, $default, $min, $max) {
 function num_input_num($name, $default, $min, $max) {
   $val = get_formint($name, $default);
   echo "<input type='number' name='$name' min='$min' value='$val' max='$max'>";
+}
+
+function print_ship_select() {
+  global $Ships;
+
+  $selected = @$_GET['ship'];
+  if (!$selected) {
+    $selected = "Anaconda";
+  }
+
+  foreach ($Ships as $ship => $csv) {
+    echo "<option value='$ship'";
+    if ($selected == $ship) {
+      echo ' selected';
+    }
+    echo ">$ship</option>";
+  }
 }
 
 $about = empty($_GET);
@@ -306,6 +337,80 @@ $about = empty($_GET);
         color: rgb(255,140,13);
         text-decoration: underline;
       }
+
+      /* Modified from https://github.com/filamentgroup/select-css */
+/* class applies to select element itself, not a wrapper element */
+.select-css {
+  display: block;
+  font-size: 16px;
+  font-family: sans-serif;
+  font-weight: 700;
+  color: #ddd;
+  line-height: 1.3;
+  padding: .6em 1.4em .5em .8em;
+  width: 100%;
+  max-width: 100%; /* useful when width is set to anything other than 100% */
+  box-sizing: border-box;
+  margin: 0;
+  border: 1px solid rgb(245,59,0);
+  box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
+  border-radius: .5em;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+  background-color: rgb(38, 25, 0);
+  /* note: bg image below uses 2 urls. The first is an svg data uri for the arrow icon, and the second is the gradient. 
+    for the icon, if you want to change the color, be sure to use `%23` instead of `#`, since it's a url. You can also swap in a different svg icon or an external image reference
+    
+  */
+  background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+    linear-gradient(to bottom, rgb(38, 25, 0) 0%,rgb(38, 25, 0) 100%);
+  background-repeat: no-repeat, repeat;
+  /* arrow icon position (1em from the right, 50% vertical) , then gradient position*/
+  background-position: right .7em top 50%, 0 0;
+  /* icon size, then gradient */
+  background-size: .65em auto, 100%;
+}
+/* Hide arrow icon in IE browsers */
+.select-css::-ms-expand {
+  display: none;
+}
+/* Hover style */
+.select-css:hover {
+  border-color: rgb(245,59,0);
+}
+/* Focus style */
+.select-css:focus {
+  border-color: rgb(245,59,0);
+  /* It'd be nice to use -webkit-focus-ring-color here but it doesn't work on box-shadow */
+  box-shadow: 0 0 1px 3px rgb(245,59,0, .7);
+  box-shadow: 0 0 0 3px -moz-mac-focusring;
+  color: #ddd;
+  outline: none;
+}
+
+/* Set options to normal weight */
+.select-css option {
+  font-weight:normal;
+}
+
+/* Support for rtl text, explicit support for Arabic and Hebrew */
+*[dir="rtl"] .select-css, :root:lang(ar) .select-css, :root:lang(iw) .select-css {
+  background-position: left .7em top 50%, 0 0;
+  padding: .6em .8em .5em 1.4em;
+}
+
+/* Disabled styles */
+.select-css:disabled, .select-css[aria-disabled=true] {
+  color: graytext;
+  background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22graytext%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+    linear-gradient(to bottom, #ffffff 0%,#e5e5e5 100%);
+}
+
+.select-css:disabled:hover, .select-css[aria-disabled=true] {
+  border-color: #aaa;
+}
+
     </style>
 
     <script>
@@ -396,6 +501,12 @@ $about = empty($_GET);
         </fieldset>
 
         <fieldset><legend>Defender</legend>
+          <label>Ship
+            <select name="ship" class="select-css">
+              <?php print_ship_select(); ?>
+              <option disabled>More Soon!</option>
+            </select>
+          </label><br>
           <label>Shield Boosters<br>
             <input type="range" name="boosters" value="<?php formint('boosters', 2) ?>" min="1" max="8">
           </label><br>
@@ -420,9 +531,6 @@ $about = empty($_GET);
           <button type="submit">Calculate</button>
           <button type="reset">Reset</button>
           <button type="button" onclick="window.location.href='<?php echo OWN_URL ?>'">Restart</button>
-          <p style="font-size: smaller">
-            Figures based on an Anaconda with size 7 shields. Others coming soon.
-          </p>
         </fieldset>
 
 <?php
